4f92cb59341fddb817470c9d7aaa5abb303b5082
Emil <emil@brannstrom.dev>
2021-12-04 13:51:53 +0100

    D3P1 done

--- 2021/3/binary_diagnostics.py
+++ 2021/3/binary_diagnostics.py
@@ -14,22 +14,26 @@ class BinaryNumber:
 class Diagnostics:
     def __init__(self, binaries):
         self.binaries = binaries
-        self.sums = [0] * len(binaries[0].as_arr)
+        self.bit_counts = [0] * len(binaries[0].as_arr)
+        self.cutoff = self.get_cutoff()
         self.result = self.diagnose()
+
+    def get_cutoff(self):
+        return len(self.binaries) / 2
 
     def diagnose(self):
         for bin in self.binaries:
-            for i in range(0, len(self.sums) - 1):
-                self.sums[i] = self.sums[i] + bin.as_arr[i]  
+            for i in range(0, len(self.bit_counts)):
+                self.bit_counts[i] = self.bit_counts[i] + bin.as_arr[i]
         gamma = ""
-        half = len(self.binaries) / 2
-        for sum in self.sums:
-            if sum >= half:
+        for sum in self.bit_counts:
+            if sum > self.cutoff:
                 gamma = gamma + "1"
             else:
                 gamma = gamma + "0"
         epsilon = gamma.replace('0', 'x').replace('1', '0').replace('x','1')
         return int(gamma, 2) * int(epsilon, 2)    
+
 
 def parse_input(path):
     return [BinaryNumber(line) for line in get_input(path)]
